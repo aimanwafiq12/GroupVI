7c3bcc91b512a067f8e55a052238367fc93b810f
aimanwafiq12 <80379435+aimanwafiq12@users.noreply.github.com>
2021-05-20 23:50:51 +0800

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,11 +1,20 @@
 package com.GroupProjectAssignment;
 
+import java.util.Random;
+
 public class Vertex <T extends Comparable<T>, N extends Comparable <N>> {
     T vertexInfo;
     int indeg;
     int outdeg;
     Vertex<T,N> nextVertex;
     Edge<T,N> firstEdge;
+    int rep = 1;
+    int dive;
+    int lunchStart;
+    int lunchPeriod;
+
+
+    Random r = new Random();
 
     public Vertex() {
         vertexInfo=null;
@@ -21,5 +30,54 @@ public class Vertex <T extends Comparable<T>, N extends Comparable <N>> {
         outdeg=0;
         nextVertex = next;
         firstEdge = null;
+
+        //Initialize these variables for each student as below:
+        // 1 <= rep <= 10,
+        // 0 < Diving rate (%) < 100,
+        dive = r.nextInt(100)+1;
+
+        // 1100 <= lunch time <= 1400,
+        lunchStart = generateTime();
+        int a = r.nextInt(60+1-5)+5;
+        while(a%10!=0){
+            a = r.nextInt(60+1-5)+5;
+        }
+        int afterPeriod = a + lunchStart;
+        if(afterPeriod>1360){
+            lunchPeriod = 1360 - lunchStart;
+        }
+        else{
+            lunchPeriod = a;
+        }
+        // 5 < lunch period(minutes) < 60,
+        // a list of friends as shown in Figure 1 below with their reputation relative to this person.
+    }
+
+    public int generateTime(){
+        Boolean check = false;
+        int a=0;
+        while(!check){
+            a = r.nextInt(1400+1-1100) + 1100;
+            if(a>=1160&&a<1200){
+                continue;
+            }
+            else if(a>=1260&&a<1300){
+                continue;
+            }
+            else if(a>=1360&&a<1400){
+                continue;
+            }
+            else if(a%10!=0){
+                continue;
+            }
+            else
+                break;
+        }
+        return a;
+    }
+
+    public int totalRep(int a){
+        rep = rep + a;
+        return rep;
     }
 }
